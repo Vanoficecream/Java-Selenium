9b2b883460bd6cc840de6fd4d1b7ef94faddfa94
Yuriy <Vanoficecream@gmail.com>
2023-06-02 05:33:32 +0600

    Final commit

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # For showcase purpose only:
-#### This repository contains 5 Java + Junit 5 + Selenium tests + JDK 14 (14.0.2)
-#### For more information check pom.xml.
-#### After cloning this repository, install chromedriver of the exact version, 
-#### that will be relevant to your current Chrome browser version.
+#### Данные тесты были написаны с использованием библиотек: junit 5, Selenium. 
+#### Версия JDK на который был написан данный тест: 14.0.2
+#### Все библиотеки находятся в pom.xml.
+#### После клонирование репозитория, скачайте подходящую версию chromedriver под ваш браузер хром: https://chromedriver.chromium.org/downloads
+#### Chromedriver.exe необходимо поместить в папку с тестами , либо во время инициализации прописать полный путь.
+#### Если по какой-то причине тест не запускается, вот ссылка на видео с его демонстрацией: https://drive.google.com/file/d/1-oeSRupzYL3dFMR0gaAmDNNiyEaE1oSE/view?usp=sharing
--- src/test/java/SeleniumExampleTest.java
+++ src/test/java/SeleniumExampleTest.java
@@ -1,10 +1,8 @@
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.devtools.DevTools;
-import org.openqa.selenium.devtools.v109.network.Network;
-import java.util.Optional;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SeleniumExampleTest {
@@ -12,23 +10,25 @@ public class SeleniumExampleTest {
 
     @BeforeAll
     public static void setUp() {
-        // Set up the WebDriver instance
+        // Инициализация хром драйвера
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
     }
 
     @Test
-    public void testOpenWikipedia() {
-        // Open Wikipedia website
+    public void test1OpenWikipedia() {
+
+        // Открываем сайт.
         driver.get("https://ru.wikipedia.org");
 
-        // Find and click on search field.
+        // Создаем переменную searchField и проверяем доступно ли поле поиска.
         WebElement searchField = driver.findElement(By.xpath("/html/body/div[4]/div[1]/div[2]/div/div/form/div/input[1]"));
         Assertions.assertTrue(searchField.isEnabled(), "Search field is not clickable.");
 
-        // Input text into a search field and click search.
-        searchField.sendKeys("Греция");
+        // Вставляем текст в поле поиск и нажимаем на значок поиска.
         WebElement btnSearch = driver.findElement(By.xpath("/html/body/div[4]/div[1]/div[2]/div/div/form/div/input[4]"));
+        searchField.sendKeys("Греция");
         btnSearch.click();
 
         try {
@@ -39,20 +39,84 @@ public class SeleniumExampleTest {
     }
 
     @Test
-    public void testLinkAvailability() {
+    public void test2LinkAvailability() {
 
-        // Enabling dev tools
-        DevTools devTools = ((ChromeDriver) driver).getDevTools();
-        devTools.createSession();
+        WebElement element = driver.findElement(By.id("n-mainpage-description"));
+        // Берем ссылку из элемента через tagName
+        WebElement tagName = element.findElement(By.tagName("a"));
 
-        // Dev tools
-        devTools.send(Network.enable(Optional.empty(), Optional.empty(), Optional.empty()));
+        // Берем ссылку из элемента если она есть.
+        String linkValue = null;
+        if (tagName != null)
+        {
+            linkValue = tagName.getAttribute("href");
+        }
 
-        // Finding the needed link
-        boolean isLinkFound = driver.findElement(By.partialLinkText("/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0")).isDisplayed();
-        Assertions.assertTrue(isLinkFound);
+        // Если ссылка есть, принтим ее в консоль.
+        if (linkValue != null && !linkValue.isEmpty()) {
+            System.out.println("Link value is available: " + linkValue);
+        }
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
+    @Test
+    public void test3ParCheckAvailability() {
+
+        // Берем содержание статьи из которой будем доставать <p>.
+        WebElement article1 = driver.findElement(By.id("bodyContent"));
+        // Находим первый <p> внутри этой статьи.
+        WebElement firstParagraph1 = article1.findElement(By.tagName("p"));
+        // Принтим контент самого первого <p> в консоль для проверки.
+        System.out.println("First paragraph text: \n" + firstParagraph1.getText());
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void testCheckAnyArticleContent(){
+        // Заново декларируем поле поиска и "лупу" поиска.
+        // Вставляем другое слово и нажимаем на "лупу" поиска.
+        WebElement searchField = driver.findElement(By.xpath("/html/body/div[4]/div[1]/div[2]/div/div/form/div/input[1]"));
+        WebElement btnSearch = driver.findElement(By.xpath("/html/body/div[4]/div[1]/div[2]/div/div/form/div/input[4]"));
+        searchField.sendKeys("Америка");
+        btnSearch.click();
+
+        // Берем статью и принтим первый <p>,
+        // чтобы удостовериться что есть содержание.
+        WebElement article2 = driver.findElement(By.id("bodyContent"));
+        WebElement firstParagraph2 = article2.findElement(By.tagName("p"));
+        System.out.println("Any article paragraph: \n" + firstParagraph2.getText());
+
+    }
+
+    @Test
+    public void test5ChangeLanguage() {
+
+//        // Скроллим вниз, чтобы позднее изменить язык на странице.
+//        JavascriptExecutor js = (JavascriptExecutor) driver;
+//        js.executeScript("window.scrollBy(0, 500);");
+
+        // Декларируем переменную нажимаем на элемент для смены языка.
+        WebElement linkContainer = driver.findElement(By.id("mw-panel"));
+        WebElement linkValue = linkContainer.findElement(By.xpath("//li[contains(@class, 'interlanguage-link') and contains(@class, 'interwiki-en')]/a"));
+        String linkUrl = linkValue.getAttribute("href");
+        System.out.println("English link value: \n" + linkUrl);
+        linkValue.click();
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
     @AfterAll
     public static void tearDown() {
